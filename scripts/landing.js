85b3b8319fcb7404a12d52f22992eb0a81ed28bf
paulhurrle <paul_hurrle@yahoo.com>
2017-07-02 13:51:55 -0700

    add changes to landing.js lost during previous merge

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,26 +1,11 @@
 var pointsArray = document.getElementsByClassName('point');
 
-<<<<<<< HEAD
-<<<<<<< HEAD
- var animatePoints = function(points) {
-=======
-     var points = document.getElementsByClassName('point');
-
->>>>>>> assignment8
-     var revealPoint = function(index) {
-         points[index].style.opacity = 1;
-         points[index].style.transform = "scaleX(1) translateY(0)";
-         points[index].style.msTransform = "scaleX(1) translateY(0)";
-         points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
-     };
-=======
 var revealPoint = function(node) {
     node.style.opacity = 1;
     node.style.transform = "scaleX(1) translateY(0)";
     node.style.msTransform = "scaleX(1) translateY(0)";
     node.style.WebkitTransform = "scaleX(1) translateY(0)";
 };
->>>>>>> assignment9
 
  var animatePoints = function(nodeList) {
      forEach(nodeList, revealPoint);
