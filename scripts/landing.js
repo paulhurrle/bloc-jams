20eac723f0b1a074368183ee8d3f4c2312c13123
paulhurrle <paul_hurrle@yahoo.com>
2017-07-08 20:38:10 -0700

    complete checkpoint16, refactor landing.css using jQuery

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,25 +1,23 @@
-var pointsArray = document.getElementsByClassName('point');
-
-var revealPoint = function(node) {
-    node.style.opacity = 1;
-    node.style.transform = "scaleX(1) translateY(0)";
-    node.style.msTransform = "scaleX(1) translateY(0)";
-    node.style.WebkitTransform = "scaleX(1) translateY(0)";
+var animatePoints = function() {
+    var revealPoint = function() {
+        // #7
+        $(this).css({
+            opacity: 1,
+            transform: 'scaleX(1) translateY(0)'
+        });
+    };
+    // #6
+    $.each($('.point'), revealPoint);
 };
 
- var animatePoints = function(nodeList) {
-     forEach(nodeList, revealPoint);
- };
-
- window.onload = function() {
-     if (window.innerHeight > 950) {
-         animatePoints(pointsArray);
+$(window).load(function() {
+    if ($(window).height() > 950) {
+         animatePoints();
      }
-     var sellingPoints = document.getElementsByClassName('selling-points')[0];
-     var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
-     window.addEventListener('scroll', function(event) {
-         if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
-             animatePoints(pointsArray);
+     var scrollDistance = $('.selling-points').offset().top - $(window).height() + 200;
+     $(window).scroll(function(event) {
+         if ($(window).scrollTop() >= scrollDistance) {
+             animatePoints();
          }
      });
- }
+ });
